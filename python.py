4fc44da84f98f1d9c6ca8ff8358b5b685771f4a1
Staji Wilson <stajiwilson@icloud.com>
2020-05-09 18:39:15 +0530

    Making changes

--- python.py
+++ python.py
@@ -1,16 +1,25 @@
 import re
+import webbrowser
+import sys
+import time
+#c = webbrowser.get('Google')
+filename = input("Enter your file name(.txt format):") + ".txt"
+#input the file name 
+p = input("Enter the word you want to search: ")
+#input the word you want to search
 
-filename = input("Enter your file name(.txt format) :") + ".txt"
-p = input("Enter the word you want to search : ")
+fo = open('output.html','wb')
+
 with open(filename, 'r') as f:
   lines = f.read()
-  
-  print(lines)
+  #read the file line by line
   print("\n *********Output********\n")
   for line in lines.split("."):
-        if re.match("(.*\s)"+p+"(\s.*)", line, re.DOTALL):
-            print(line+".")
-     
+      if (re.match("(?i).*\s"+p+"\s.*", line,re.DOTALL)):
+              print(line+".")
+              
+
+#webbrowser.open_new_tab('output.html')
+#c.open_new_tab('http://docs.python.org')
 
 
-    
